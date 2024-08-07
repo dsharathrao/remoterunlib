bae0874397c0ad76a3f8a7ba9aff50440b361293
Sharath Kumar <dsharathrao@gmail.com>
2024-08-07 06:29:53 +0530

    Bug fixes and optimize

--- demo/run.py
+++ demo/run.py
@@ -6,10 +6,10 @@ from remoterunlib import SSHClient
 # Example usage
 if __name__ == "__main__":
 
-    hostname = "192.168.0.105"  # Replace with your server's hostname or IP
+    hostname = "hostname or IP"  # Replace with your server's hostname or IP
     port = 22  # SSH port (usually 22)
-    username = "chinni"  # Replace with your SSH username
-    password = "wipro@11"  # Replace with your SSH password
+    username = "username"  # Replace with your SSH username
+    password = "password"  # Replace with your SSH password
 
     ssh_client = SSHClient(hostname, username, password)
     ssh_client.login()
@@ -20,11 +20,14 @@ if __name__ == "__main__":
     # result = ssh_client.run_python_file("demo/selenium_test_script.py")
     # print(f"Result from remote function: {result}")
     # ssh_client.run_command("dir")
-    ssh_client.send_File("demo/demo_sendFile.txt")
+    
+    ssh_client.send_File("demo_sendFile.txt")
     ssh_client.receive_File("C:\\temp\\sharath.txt", "sharath.txt")
     ssh_client.ping()
     ssh_client.run_command("dir")
-    ssh_client.reboot()
+    # ssh_client.reboot()
     ssh_client.run_command("dir")
-    result = ssh_client.run_powershell_command("Get-Process")
+    ssh_client1 = SSHClient(hostname, username, password)
+    result = ssh_client1.run_powershell_command("Get-Process")
     ssh_client.close()
+    ssh_client1.close()
